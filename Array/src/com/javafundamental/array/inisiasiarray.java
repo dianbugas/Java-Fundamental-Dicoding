1c8a0fcb4fac95f920cf3be957a5c43c8ffb6a06
dianynf <dianbugas@gmail.com>
2019-09-29 22:10:37 +0700

    array

---
+++ Array/src/com/javafundamental/array/inisiasiarray.java
@@ -0,0 +1,29 @@
+package com.javafundamental.array;
+
+public class inisiasiarray {
+    public static void main(String[] args) {
+        //cara no 1
+        //        int[] arrInt = new int[]{1, 2, 3, 4, 5, 6};
+//        System.out.println(arrInt[0]);
+//        System.out.println(arrInt[1]);
+//        System.out.println(arrInt[2]);
+//        System.out.println(arrInt[3]);
+//        System.out.println(arrInt[4]);
+//        System.out.println(arrInt[5]);
+    //cara no 2
+        int[] arrInt = new int[6];
+        arrInt[0] = 1;
+        arrInt[1] = 2;
+        arrInt[2] = 3;
+        arrInt[3] = 4;
+        arrInt[4] = 5;
+        arrInt[5] = 6;
+
+        System.out.println(arrInt[0]);
+        System.out.println(arrInt[1]);
+        System.out.println(arrInt[2]);
+        System.out.println(arrInt[3]);
+        System.out.println(arrInt[4]);
+        System.out.println(arrInt[5]);
+    }
+}
---
+++ Perulangan/src/com/javafundamental/perulangan/DoWhile.java
@@ -0,0 +1,11 @@
+package com.javafundamental.perulangan;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int nilai = 1;
+        do {
+            System.out.println("Angka : " + nilai);
+            nilai++;
+        } while (nilai <= 10);
+    }
+}
