bde63a74f43d73a3ae9c6462de074b97a5881645
dianynf <dianbugas@gmail.com>
2019-09-29 13:21:23 +0700

    operator

---
+++ InputOutput/src/com/javafudamental/inputouput/UserOuput.java
@@ -0,0 +1,8 @@
+package com.javafudamental.inputouput;
+
+public class UserOuput {
+    public static  void main(String [] args){
+        System.out.println("cetak gunain print ln");
+        System.out.print("cetak pakai print");
+    }
+}
---
+++ Operator/src/com/javafundamental/operator/OperatorAssingment.java
@@ -0,0 +1,11 @@
+package com.javafundamental.operator;
+
+public class OperatorAssingment {
+    public static void main(String[] args){
+        int mData1 = 2;
+        int mData2 = 19;
+        System.out.println("data pada variabel mData 1 : " + mData1);
+        System.out.println("data pada variabel mData 2 : " + mData2);
+
+    }
+}
