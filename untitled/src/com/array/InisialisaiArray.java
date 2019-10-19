01a505b70421e75b99246aa25794ec72ab859ff8
dianynf <dianbugas@gmail.com>
2019-10-19 23:49:05 +0700

    Quis

---
+++ untitled/src/com/array/InisialisaiArray.java
@@ -0,0 +1,17 @@
+package com.array;
+
+public class InisialisaiArray {
+    public static void main(String []args){
+        int [] arrInt = {1,2,3,4,5}; //new int[5]
+//        arrInt[0] = 1;
+//        arrInt[1] = 2;
+//        arrInt[2] = 3;
+//        arrInt[3] = 4;
+//        arrInt[4] = 5;
+        System.out.println(arrInt[0]);
+        System.out.println(arrInt[1]);
+        System.out.println(arrInt[2]);
+        System.out.println(arrInt[3]);
+        System.out.println(arrInt[4]);
+    }
+}
