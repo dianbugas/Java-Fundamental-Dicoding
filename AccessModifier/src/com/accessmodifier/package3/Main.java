3ff2fd275db3ae140d3fcd140a4009bcd862777e
dianynf <dianbugas@gmail.com>
2019-10-22 23:18:57 +0700

    perhitungan

---
+++ AccessModifier/src/com/accessmodifier/package3/Main.java
@@ -0,0 +1,7 @@
+package com.accessmodifier.package3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nilainya adalah " + Perhitungan.nilai);
+    }
+}
---
+++ AccessModifier/src/com/accessmodifier/package3/Perhitungan.java
@@ -0,0 +1,5 @@
+package com.accessmodifier.package3;
+
+public class Perhitungan {
+    public static int nilai = 0;
+}
