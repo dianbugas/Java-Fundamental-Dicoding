995e8ec8a971d545375e5d97ae7264b4bb6fb755
dianynf <dianbugas@gmail.com>
2019-10-23 22:39:28 +0700

    access modifier

--- AccessModifier/src/com/accessmodifier/package3/Main.java
+++ AccessModifier/src/com/accessmodifier/package3/Main.java
@@ -3,5 +3,13 @@ package com.accessmodifier.package3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Nilainya adalah " + Perhitungan.nilai);
+
+        for (int x = 0 ; x < 5; x++){
+            new Perhitungan();
+        }
+
+        System.out.println("Sampai "+Perhitungan.nilai);
+
+        System.out.println("getNilai memiliki nilai " + Perhitungan.getNilai());
     }
 }
--- AccessModifier/src/com/accessmodifier/package3/Perhitungan.java
+++ AccessModifier/src/com/accessmodifier/package3/Perhitungan.java
@@ -2,4 +2,12 @@ package com.accessmodifier.package3;
 
 public class Perhitungan {
     public static int nilai = 0;
+
+    protected static int getNilai(){
+        return nilai;
+    }
+
+    Perhitungan() {
+        nilai++;
+    }
 }
